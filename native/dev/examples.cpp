19e3c986ec9ac164c4647fcc5cbc00c622922a4a
quliikay <xuejiaqi001@gmail.com>
2023-02-06 15:32:08 -0500

    update: task 1

--- native/dev/examples.cpp
+++ native/dev/examples.cpp
@@ -20,19 +20,20 @@ int main() {
     int log_epoch;
     cin >> log_epoch;
 
-    for (int N = 1; N <= 64; N++)
-        experiment_ckks(N, N, flops, log_epoch, 0);
-    for (int N = 2; N <= 64; N++)
-        experiment_ckks(N, N - 1, flops, log_epoch, 0);
-
-    for (int N = 1; N <= 64; N++)
-        experiment_bgv(N, N, flops, log_epoch, validation);
-    for (int N = 2; N <= 64; N++)
-        experiment_bgv(N, N - 1, flops, log_epoch, validation);
-
-    for (int N = 1; N <= 64; N++)
-        experiment_bfv(N, N, flops, log_epoch, validation);
-    for (int N = 2; N <= 64; N++)
-        experiment_bfv(N, N - 1, flops, log_epoch, validation);
+    experiment_bgv(64, 64, flops, log_epoch, validation);
+//    for (int N = 1; N <= 64; N++)
+//        experiment_ckks(N, N, flops, log_epoch, 0);
+//    for (int N = 2; N <= 64; N++)
+//        experiment_ckks(N, N - 1, flops, log_epoch, 0);
+//
+//    for (int N = 1; N <= 64; N++)
+//        experiment_bgv(N, N, flops, log_epoch, validation);
+//    for (int N = 2; N <= 64; N++)
+//        experiment_bgv(N, N - 1, flops, log_epoch, validation);
+//
+//    for (int N = 1; N <= 64; N++)
+//        experiment_bfv(N, N, flops, log_epoch, validation);
+//    for (int N = 2; N <= 64; N++)
+//        experiment_bfv(N, N - 1, flops, log_epoch, validation);
     return 0;
 }
